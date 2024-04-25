682dc8a1213671d138a1811f536e8a194d1b872b
venkat <venkat.prabhu@10decoders.in>
2024-04-25 18:04:34 +0530

    initial-commit

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { StarComponent } from './star.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
+  declarations: [
+    StarComponent
+  ],
+  exports: [
+    StarComponent,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ]
+})
+export class SharedModule { }
